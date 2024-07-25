4a9e74bc987dbd5bc07d55cc70a771a7183bbe72
Kiki <ismajlikiki@gmail.com>
2024-07-24 19:49:45 -0500

    Updates

---
+++ precache-manifest.9b06a8fc1390bc408d747976f1c8244a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a1a25e307db002e1e387c1edd4c2b24",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6cce3ef6f257a24cf3a0",
+    "url": "/static/css/main.f2b4f123.chunk.css"
+  },
+  {
+    "revision": "ee63e87fde5663a9e846",
+    "url": "/static/js/2.60985f6c.chunk.js"
+  },
+  {
+    "revision": "de6bb9b182cb4883b744809bb1ef7215",
+    "url": "/static/js/2.60985f6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6cce3ef6f257a24cf3a0",
+    "url": "/static/js/main.dc239adc.chunk.js"
+  },
+  {
+    "revision": "243436cac8849d74e94a",
+    "url": "/static/js/runtime-main.df72bfa8.js"
+  }
+]);
